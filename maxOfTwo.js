e84bc375f90df7416c7df41af9c6df0ad1146ef1
Logan Leatherow <lleatherow@niagarawater.com>
2022-04-16 13:08:44 -0500

    Finished

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -1,5 +1,15 @@
-let x = 1
-let y = 2
+let x = 5
+let y = 5
 
 // Write your code below this line.
-console.log(x)
+if ( x > y) {
+    console.log (x)
+}
+else if (x < y) {
+    console.log (y)
+}
+    else {console.log("They are the same")
+    }
+
+
+
